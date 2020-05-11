32a18ab1cffff81e4929133f28c1dbcf4d712707
Benjamin Trainor <benjamin.trainor.js@protonmail.com>
2020-05-11 23:44:11 +0930

    added audio files for bomb and win, added reset button, added new styling

--- minesweeper.js
+++ minesweeper.js
@@ -1,176 +1,246 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-  cells: [
-    // row 1
-    {
-      row: 0,
-      col: 0,
-      isMine: true,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 0,
-      col: 1,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 0,
-      col: 2,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 0,
-      col: 3,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    // row 2
-    {
-      row: 1,
-      col: 0,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 1,
-      col: 1,
-      isMine: true,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 1,
-      col: 2,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 1,
-      col: 3,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    // row 3
-    {
-      row: 2,
-      col: 0,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 2,
-      col: 1,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 2,
-      col: 2,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 2,
-      col: 3,
-      isMine: true,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    // row 4
-    {
-      row: 3,
-      col: 0,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 1,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 2,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    },
-    {
-      row: 3,
-      col: 3,
-      isMine: false,
-      isMarked: false,
-      hidden: true,
-      surroundingMines: 0
-    }
-  ] 
+// var board = {
+//   cells: [
+//     // row 1
+//     {
+//       row: 0,
+//       col: 0,
+//       isMine: false, 
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 0,
+//       col: 1,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 0,
+//       col: 2,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 0,
+//       col: 3,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     // row 2
+//     {
+//       row: 1,
+//       col: 0,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 1,
+//       col: 1,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 1,
+//       col: 2,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 1,
+//       col: 3,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     // row 3
+//     {
+//       row: 2,
+//       col: 0,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 2,
+//       col: 1,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 2,
+//       col: 2,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 2,
+//       col: 3,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     // row 4
+//     {
+//       row: 3,
+//       col: 0,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 1,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 2,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     },
+//     {
+//       row: 3,
+//       col: 3,
+//       isMine: false,
+//       isMarked: false,
+//       hidden: true,
+//       surroundingMines: 0
+//     }
+//   ] 
+// }
+
+
+// Function to create the individual board objects
+function cellCreator(cellRow, cellCol) {
+  return {
+    row: cellRow,
+    col: cellCol,
+    isMine: Math.random() > 0.2 ? false : true,
+    isMarked: false,
+    hidden: true,
+  }
 }
 
+// Function to pass individual board objects into board.cells array
+function boardCreator (xyAxis) {
+
+  var tempArray = [];
+  
+  i = 0;
+
+  for (let y = 0; y < xyAxis; y++) {
+    for (let x = 0; x < xyAxis; x++) {
+      tempArray[i] = cellCreator(y, x);
+      i++;
+    }
+  }
+  return tempArray;
+}
+
+let board;
+let difficulty = 6;
+
+
+
+
+
+
 function startGame () {
+
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
+
+
+  board = { cells: boardCreator(difficulty)};
+
+  // setMines();
+
   for (let i = 0; i < board['cells'].length; i++){
     var cellPlaceHldr = board.cells[i]
     board.cells[i].surroundingMines = countSurroundingMines (cellPlaceHldr);
   }
 
-  document.addEventListener("click", checkForWin);
-  document.addEventListener("contextmenu", checkForWin);
+  
+
 
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
+
+
+
+// function setMines () {
+//   for (let i = 0; i < board['cells'].length; i++){
+//     var value = 0;
+//     value = Math.floor(Math.random() * 10);
+//     if (value < 2) {
+//       board['cells'][i].isMine = true;
+//     } 
+//   }
+// }
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-  let hasWon = false;
   for (let i = 0; i < board['cells'].length; i++){
-    if (board.cells[i].isMine === true && 
-      board.cells[i].isMarked === true) {
-      hasWon = true;
-    } else if (board.cells[i].isMine === true && 
-      board.cells[i].isMarked === false) {
-      hasWon = false;
-    } else if (board.cells[i].hidden === true) {
-      hasWon = false;
+    
+    if ((board.cells[i].isMine === true) && 
+      (board.cells[i].isMarked === false)) {
+      return;
+    } else if ((board.cells[i].hidden === true) && (board.cells[i].isMine === false)) {
+      return;
     }
+    // else if (board.cells[i].isMine === true && 
+    //   board.cells[i].isMarked === true) {
+    //   hasWon = true;
+    // } 
+    
+    // if (board.cells[i].isMine === true && 
+    //   board.cells[i].isMarked === true) {
+    //   hasWon = true;
+    // } else if (board.cells[i].isMine === true && 
+    //   board.cells[i].isMarked === false) {
+    //   hasWon = false;
+    // } else if (board.cells[i].hidden === true) {
+    //   hasWon = false;
+    // }
   }
-  if (hasWon === true) {
-    lib.displayMessage('You win!');
-  }
+  lib.displayMessage('You win!');
+  let sound = new Audio("./sound/heavenly.mp3");
+    sound.volume = 0.4;
+    sound.play();
+  
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -186,9 +256,22 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
-  let row = cell.row;
-  let col = cell.col;
-  var surroundingCells = getSurroundingCells(row, col);
+
+    //var count = 0
+
+  var surroundingCells = getSurroundingCells(cell.row, cell.col);
+
+// // for (var i = 0; surrounding.length; i++) {
+//     var cell = surrounding[i]
+
+//     if (cell.isMine) {
+//         count++
+//     }
+// }
+
+
+// return count;
+
   let num = surroundingCells.filter(isMine);
 
   return num.length;
@@ -206,3 +289,6 @@ function countSurroundingMines (cell) {
 
 }
 
+function gameReset () {
+  location.reload();
+}
